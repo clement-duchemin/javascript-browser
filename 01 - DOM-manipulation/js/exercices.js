d5772e1fe628385bfa72c1c09055d909fff7e45e
Clement <clementduchemin19@gmail.com>
2023-09-12 12:35:13 +0200

    first-commit

--- 01 - DOM-manipulation/js/exercices.js
+++ 01 - DOM-manipulation/js/exercices.js
@@ -1,1 +1,48 @@
-// Your code goes here
+/* //1
+
+let paragraphe = document.getElementById('monParagraphe');
+paragraphe.textContent = "Bonjour, DOM !";
+//paragraphe.innerText = "Bonjour, DOM !";
+
+function addTextInside(target, contenu){
+    target.innerText = contenu;
+}
+
+addTextInside(paragraphe, "hi from function");
+
+
+
+//2
+let image = document.getElementById('monImage');
+image.src = "images/img2.jpg";
+
+
+
+//3
+let parentDiv = document.getElementById('elementParent');
+let button = document.getElementById('myButton');
+parentDiv.appendChild(button);
+
+//4
+let classCss = document.querySelector('.to-be-removed');
+classCss.classList.add('new-one');
+classCss.classList.remove('to-be-removed');
+
+//5
+const noms = ["Alice", "Bob", "Charlie"];
+
+const liste = document.getElementById("myList");
+
+for (let nom of noms) {
+  let li = document.createElement("li");
+  li.textContent = nom;
+  liste.appendChild(li);
+}
+
+//6
+let liRemoved = document.getElementById('exo-6-list').getElementsByTagName('li')[1];
+liRemoved.remove();
+
+
+
+ */
--- 02 - events-management/js/exercices.js
+++ 02 - events-management/js/exercices.js
@@ -1,1 +1,33 @@
-// Your code goes here
+//1
+
+let btnClick = document.getElementById('button-2-1');
+
+btnClick.addEventListener('click', function() {
+    alert('hey');
+});
+
+
+
+//2
+
+let hoverMe = document.getElementById('hover-me');
+
+hoverMe.addEventListener('mouseover', function(){
+    hoverMe.style.color='red';
+});
+hoverMe.addEventListener('mouseleave', function(){
+    hoverMe.style.color='green';
+});
+
+
+//3
+
+let inputSelection = document.getElementById('myInput');
+
+inputSelection.addEventListener('keydown', function(event){
+    console.log('Touche appuyée :', event.key);
+})
+
+//4
+
+let dialogue = document.getElementById('maBoiteDialogueUnique');
